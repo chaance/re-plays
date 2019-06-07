ee1e835883bc58848ed5f08e335598a829a54ea1
Chance Strickland <hi@chancedigital.io>
2019-06-06 22:50:39 -0700

    formatting test

--- src/AudioPlayer.js
+++ src/AudioPlayer.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import cx from 'classnames';
 
+
+
+
+
+
 const SOME_STATE_LIKE_THING = {}; // may need a reducer, may not? GUESS WE WILL SEE
 const formatTime = () => {}; // format MS to HH:MM:SS
+
+
 const LANGUAGE_CODE = 'https://r12a.github.io/app-subtags/'; // << GET LIST FOR THIS TYPE
 
 const AudioPlayer = ({

3f85f595b113fc15b670b5e92ba57f41dcb92266
Chance Strickland <hi@chancedigital.io>
2019-06-06 19:25:05 -0700

    init

---
+++ AudioPlayer.js
@@ -0,0 +1,289 @@
+import React from "react";
+import cx from "classnames";
+
+const SOME_STATE_LIKE_THING = {}; // may need a reducer, may not? GUESS WE WILL SEE
+const formatTime = () => {}; // format MS to HH:MM:SS
+const LANGUAGE_CODE = "https://r12a.github.io/app-subtags/"; // << GET LIST FOR THIS TYPE
+
+const AudioPlayer = ({
+  src, // required. string | string[] | { src: string, type?: https://tools.ietf.org/html/rfc4281 }[]
+  duration = 100, // number
+  captions = [{}], // { src: string,  }[]
+  crossOrigin,
+  autoPlay = false,
+  loop = false,
+  muted = false,
+  preLoad = "metadata"
+}) => {
+  const {
+    isPlaying = false,
+    isScrubbing = false,
+    volumeControlIsActive = false,
+    volumeLevel = 1,
+    ...state
+  } = SOME_STATE_LIKE_THING;
+  const elapsedTime = formatTime(state.elapsedTime);
+  const remainingTime = formatTime(duration - elapsedTime);
+
+  const handleAutoPlay = () => {};
+  const handleLoop = () => {};
+  const handleMute = () => {};
+
+  return (
+    <div
+      pseudo="-webkit-media-controls"
+      className={cx(
+        "phase-ready",
+        `state-${isScrubbing ? "scrubbing" : isPlaying ? "playing" : "stopped"}`
+      )}
+    >
+      {/* some props should stay false and remain controlled by the state */}
+      {/* autoPlay, loop, muted */}
+      <audio
+        className="HIDE_ME"
+        autoPlay={
+          false /* This should be controled by component state, so always keep it false here */
+        }
+        crossOrigin={
+          crossOrigin /* May need to manually determine if the request is successful if this is set? */
+        }
+        loop={false}
+        muted={false}
+        preLoad={preLoad}
+        src={typeof src === "string" && src}
+      >
+        {Array.isArray(src) &&
+          src.map((source, i) => (
+            <source
+              key={i}
+              src={typeof source === "string" ? source : source.src}
+              type={typeof source !== "string" && source.type}
+            />
+          ))}
+      </audio>
+      {captions.length &&
+        captions
+          .filter(caption => !!caption.src)
+          .map((caption, i) => (
+            <video
+              key={i}
+              controls={false}
+              width="MAKE_DYNAMIC_INT"
+              height="MAKE_DYNAMIC_INT"
+              src={typeof src === "string" && src}
+              muted={true} // sync with audio state
+              preLoad="none" // should be handled by audio preLoad? Will need to test this.
+              loop={false} // sync with audio state
+              autoPlay={false} // sync with audio state
+            >
+              {Array.isArray(src) &&
+                src.map((source, i) => (
+                  <source
+                    key={i}
+                    src={typeof source === "string" ? source : source.src}
+                    type={typeof source !== "string" && source.type}
+                  />
+                ))}
+              <track
+                label={caption.label}
+                kind="subtitles"
+                srcLang={
+                  caption.srcLang /* two-letter language code: https://r12a.github.io/app-subtags/ */
+                }
+                src={caption.src}
+                default={caption.default}
+              />
+            </video>
+          ))}
+      <div pseudo="-webkit-media-controls-enclosure">
+        <div pseudo="-webkit-media-controls-panel">
+          <button
+            type="button"
+            pseudo="-webkit-media-controls-play-button"
+            className={cx({ pause: !isPlaying })}
+          >
+            <span className="HIDE_ME">{isPlaying ? "pause" : "play"}</span>
+            <div pseudo="-internal-media-controls-button-hover-background" />
+          </button>
+
+          <div
+            aria-label={`elapsed time: ${elapsedTime}`}
+            pseudo="-webkit-media-controls-current-time-display"
+          >
+            <span>{elapsedTime}</span>
+          </div>
+          <div
+            aria-label={`remaining time: / ${remainingTime}`}
+            pseudo="-webkit-media-controls-time-remaining-display"
+          >
+            <span>/ {remainingTime}</span>
+          </div>
+          <input
+            type="range"
+            step="any"
+            pseudo="-webkit-media-controls-timeline"
+            max={duration / 100}
+            aria-valuetext="0:00"
+            className="HIDE_ME"
+          />
+          <div className="VISUAL_TRACK" aria-hidden>
+            <div pseudo="-internal-media-controls-segmented-track" id="track">
+              <div id="thumb" />
+              <div pseudo="-internal-track-segment-background">
+                <div pseudo="-internal-track-segment-highlight-before" />
+                <div pseudo="-internal-track-segment-highlight-after" />
+              </div>
+            </div>
+          </div>
+          <div
+            pseudo="-webkit-media-controls-volume-control-container"
+            className={cx({ closed: !volumeControlIsActive })}
+          >
+            <div pseudo="-webkit-media-controls-volume-control-hover-background" />
+            <input
+              type="range"
+              step="any"
+              max={1}
+              aria-valuemax={100}
+              aria-valuemin={0}
+              aria-label="volume"
+              pseudo="-webkit-media-controls-volume-slider"
+              aria-valuenow={Math.floor(volumeLevel * 100)}
+              className={cx({ closed: !volumeControlIsActive })}
+            />
+            <button type="button" pseudo="-webkit-media-controls-mute-button">
+              <span className="HIDE_ME">
+                {volumeLevel === 0 ? "unmute" : "mute"}
+              </span>
+            </button>
+          </div>
+          <input
+            type="button"
+            pseudo="-webkit-media-controls-fullscreen-button"
+            aria-label="enter full screen"
+            style="display: none;"
+          />
+          <input
+            type="button"
+            aria-label="show more media controls"
+            title="more options"
+            pseudo="-internal-media-controls-overflow-button"
+          />
+        </div>
+      </div>
+      <div
+        role="menu"
+        aria-label="Options"
+        pseudo="-internal-media-controls-text-track-list"
+        style="display: none;"
+      />
+      <div
+        pseudo="-internal-media-controls-overflow-menu-list"
+        role="menu"
+        className="closed"
+        style="display: none;"
+      >
+        <label
+          pseudo="-internal-media-controls-overflow-menu-list-item"
+          role="menuitem"
+          tabIndex="0"
+          aria-label="Play"
+          style="display: none;"
+        >
+          <input
+            type="button"
+            pseudo="-webkit-media-controls-play-button"
+            tabIndex="-1"
+            aria-label="play"
+            className="pause"
+            style="display: none;"
+          />
+          <div aria-hidden="true">
+            <span>Play</span>
+          </div>
+        </label>
+        <label
+          pseudo="-internal-media-controls-overflow-menu-list-item"
+          role="menuitem"
+          tabIndex="0"
+          aria-label="enter full screen Fullscreen "
+          style="display: none;"
+        >
+          <input
+            type="button"
+            pseudo="-webkit-media-controls-fullscreen-button"
+            aria-label="enter full screen"
+            tabIndex="-1"
+            style="display: none;"
+          />
+          <div aria-hidden="true">
+            <span>Fullscreen</span>
+          </div>
+        </label>
+        <label
+          pseudo="-internal-media-controls-overflow-menu-list-item"
+          role="menuitem"
+          tabIndex="0"
+          aria-label="download media Download "
+          className="animated-0"
+        >
+          <input
+            type="button"
+            aria-label="download media"
+            pseudo="-internal-media-controls-download-button"
+            tabIndex="-1"
+          />
+          <div aria-hidden="true">
+            <span>Download</span>
+          </div>
+        </label>
+        <label
+          pseudo="-internal-media-controls-overflow-menu-list-item"
+          role="menuitem"
+          tabIndex="0"
+          aria-label=" Mute "
+          className="animated-2"
+          style="display: none;"
+        >
+          <button
+            type="button"
+            pseudo="-webkit-media-controls-mute-button"
+            tabIndex="-1"
+            style="display: none;"
+          >
+            <span>{volumeLevel === 0 ? "Unmute" : "Mute"}</span>
+          </button>
+        </label>
+        <label
+          pseudo="-internal-media-controls-overflow-menu-list-item"
+          role="menuitem"
+          tabIndex="0"
+          aria-label="play on remote device Cast "
+          className="animated-1"
+          style="display: none;"
+        />
+        <label
+          pseudo="-internal-media-controls-overflow-menu-list-item"
+          role="menuitem"
+          tabIndex="0"
+          aria-label="show closed captions menu Captions "
+          className="animated-0"
+          style="display: none;"
+        >
+          <button
+            aria-label="show closed captions menu"
+            type="button"
+            pseudo="-webkit-media-controls-toggle-closed-captions-button"
+            className="closed-captions"
+            tabIndex="-1"
+            style="display: none;"
+          >
+            <span>Captions</span>
+          </button>
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default AudioPlayer;
---
+++ README.md
@@ -0,0 +1,52 @@
+# React Audio Player
+
+Just an idea right now. Sketching out ideas for the markup in `AudioPlayer.js`. Trying to mirror the HTML audio element's API and shadow DOM (a la Chrome) as a general model, with some enhancements (audio support for captions via the track element). This will be written in TypeScript.
+
+## General thoughts
+- Props should be close to 1-to-1 with HTML5 audio. Some exceptions:
+  - Add a `duration` prop. We don't want to force users to fetch data about an audio file. Users should be able to make that decision.
+  - No controls prop. If the user doesn't want controls, they can just use an HTML audio tag.
+  - `src` can be a string or an array containing multiple source files (see type annotations in code comments)
+    - Array of strings: `[ 'file.mp3', 'file.wav' ]`
+    - Array of objects: `[ { type: 'audio/mp3', src: 'file.mp3' }, { type: 'audio/wav', src: 'file.wav' } ]`
+      - See: https://tools.ietf.org/html/rfc4281
+  - May need to research and test effects of `crossOrigin` and potential bugs related to component state
+  - Captions should be implemented by showing a dynamic HTML5 video with a track tag.
+    - `captions` should accept an array of caption objects for multi-language support
+      - `{ src: string, label: string, srcLang: LANGUAGE_CODE, default?: boolean (only one track can be default) }[]`
+
+## Component API ideas
+I like Formik as a model. A user can specify an `AudioPlayer` component alone and let the component handle the rest, OR we can break it down to accept a render prop and/or children to let the user render various sub-components as they see fit.
+
+```js
+<AudioPlayer src={audioFile} {...props} />
+
+// OR
+<AudioPlayer src={audioFile}>
+  <Scrubber />
+  <VolumeControl />
+  <PlayControls>
+    <BackButton />
+    <PlayButton />
+    <ForwardButton />
+  </PlayControls>
+</AudioPlayer>
+
+// OR
+<AudioPlayer
+  src={audioFile}
+  render={({
+    play,
+    pause,
+    isPlaying,
+    duration,
+    elapsedTime,
+    remainingTime
+  }) => (
+    <div>
+      <button onClick={isPlaying ? pause : play}>{isPlaying ? 'Pause' : 'Play'}</button>
+      <span>{elapsedTime} of {duration}; only {remainingTime} left to go!</span>
+    </div>
+  )}
+/>
+```
